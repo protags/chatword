aec02d1649dd01f7585dec32b1566f6bbfa8afe6
aryankhairnar <aryankhairnar9822@gmail.com>
2026-07-19 21:01:33 +0530

    feat: add Docker support and enable static file serving for the frontend in the backend server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,15 @@
 import express from "express"
 import "dotenv/config"
 import cors from "cors"
-import http from "http"
 import { clerkMiddleware } from "@clerk/express"
 import { connectDB } from "./libs/db.js";
+import fs from "fs"
+import path from "path"
 
 const PORT = process.env.PORT || 5000;
 const FRONTEND_URL = process.env.FRONTEND_URL
+const publicDir = path.join(process.cwd(), "public")
+
 
 const corsOptions = {
     origin: FRONTEND_URL,
@@ -22,6 +25,13 @@ app.use(clerkMiddleware())
 app.get("/health", (req, res) => {
     res.status(200).json({ success: true })
 })
+
+if (fs.existsSync("public")) {
+    app.use(express.static(publicDir))
+    app.get("/{*any}", (req, res, next) => {
+        res.sendFile(path.join(publicDir, "index.html"), (err) => next(err))
+    })
+}
 
 app.listen(PORT, () => {
     connectDB()
