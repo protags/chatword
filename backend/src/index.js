041457542eab6bfb431b1e775fca8fc2c74a1562
aryankhairnar <aryankhairnar9822@gmail.com>
2026-07-19 21:56:43 +0530

    fix: resolve public directory path resolution using import.meta.dirname

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ import path from "path"
 
 const PORT = process.env.PORT || 5000;
 const FRONTEND_URL = process.env.FRONTEND_URL
-const publicDir = path.join(process.cwd(), "public")
+const publicDir = path.join(import.meta.dirname, "..", "public")
 
 
 const corsOptions = {
@@ -26,7 +26,7 @@ app.get("/health", (req, res) => {
     res.status(200).json({ success: true })
 })
 
-if (fs.existsSync("public")) {
+if (fs.existsSync(publicDir)) {
     app.use(express.static(publicDir))
     app.get("/{*any}", (req, res, next) => {
         res.sendFile(path.join(publicDir, "index.html"), (err) => next(err))
